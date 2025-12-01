722267df0d5ddf902903ca2b402f2beeb65a5ed1
ilhamelin <ilhamelin5@gmail.com>
2025-12-01 18:38:23 -0300

    feat: implement initial website structure with core pages, components, and global styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,15 @@ import react from "@astrojs/react"; //
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://meridiancore.com',
+
+
     vite: {
         plugins: [tailwindcss()],
+        server: {
+            // Esto permite que el túnel de Cloudflare acceda a tu PC
+            allowedHosts: true,
+        }
     },
     integrations: [
         react() // <--- ¿ESTA FUNCIÓN ESTÁ AQUÍ?
